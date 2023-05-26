fc35ba1409d22ba7f8f2c63f25740b75d0ca4d4f
lamchachti <115954941+lamchachti@users.noreply.github.com>
2023-05-26 18:35:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function () {
+
+    $('.menu').click(function() {
+        $('.overlay').toggleClass('anim');
+        $(this).addClass('open')
+    });
+
+    $('.open').click(function(){
+        $('.overlay').toggleClass('reverse-animation');
+    })
+});
+
